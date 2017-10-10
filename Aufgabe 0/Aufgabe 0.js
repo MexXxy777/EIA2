f65a65b79478d51498d82b0e6a9e9821b16064b4
braun <braun@LAPTOP-V4RK3V37.local>
2017-10-10 23:00:07 +0200

    Kommentar

--- Aufgabe 0/Aufgabe 0.js
+++ Aufgabe 0/Aufgabe 0.js
@@ -1,8 +1,16 @@
-function eingabe() {
-    var eingabe = prompt("Wie heisst du?", "Kevin, Dieter, Klaus...");
-    if (eingabe != null) {
+/*
+Aufgabe: (00 Miniseite)
+Name: (Maximilian Braun)
+Matrikel: (256301)
+Datum: (10.10.2017)
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+function input() {
+    var input = prompt("Wie heisst du?", "Kevin, Dieter, Klaus...");
+    if (input != null) {
         document.getElementById("greet").innerHTML =
-            "Servus " + eingabe + "! Wie gehts dir?";
+            "Servus " + input + "! Wie gehts dir?";
     }
 }
 //# sourceMappingURL=Aufgabe 0.js.map
--- Aufgabe 0/Aufgabe 0.ts
+++ Aufgabe 0/Aufgabe 0.ts
@@ -1,7 +1,17 @@
-function eingabe() {
-    var eingabe = prompt("Wie heisst du?", "Kevin, Dieter, Klaus...");
-    if (eingabe != null) {
+/*
+Aufgabe: (00 Miniseite)
+Name: (Maximilian Braun)
+Matrikel: (256301)
+Datum: (10.10.2017)
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+function input() {
+    var input = prompt("Wie heisst du?", "Kevin, Dieter, Klaus...");
+    if (input != null) {
         document.getElementById("greet").innerHTML =
-            "Servus " + eingabe + "! Wie gehts dir?";
-        
-        }}
+            "Servus " + input + "! Wie gehts dir?";
+
+    }
+}
