401ee4bdb6011ed8ffe08678d46f96f27f7eeb28
braun <braun@LAPTOP-V4RK3V37.eduroam.hs-furtwangen.de>
2017-11-02 10:31:16 +0100

    Animation Schneeflocken

--- Aufgabe 03/skipiste.js
+++ Aufgabe 03/skipiste.js
@@ -9,11 +9,18 @@ var aufgabe3;
 (function (aufgabe3) {
     window.addEventListener("load", init);
     let crc2;
+    let x = 250;
+    let y = 250;
+    let arrayX = [];
+    let arrayY = [];
+    var canvasImg;
     function init() {
         let canvas = document.getElementsByTagName("canvas")[0];
         console.log(canvas);
         crc2 = canvas.getContext("2d");
         console.log(crc2);
+        console.log("setTimeout");
+        /*Himmel - Canvas breiter als Aufgabenstellung*/
         crc2.fillStyle = "#b2dfee";
         crc2.fillRect(0, 0, 1000, 600);
         /*Piste*/
@@ -24,40 +31,6 @@ var aufgabe3;
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = "#ffffff";
-        crc2.fill();
-        /*B�ume*/
-        crc2.fillStyle = "brown";
-        crc2.fillRect(730, 580, 10, 20);
-        crc2.beginPath();
-        crc2.moveTo(735, 500);
-        crc2.lineTo(710, 560);
-        crc2.lineTo(760, 560);
-        crc2.closePath();
-        crc2.fillStyle = "darkgreen";
-        crc2.fill();
-        crc2.beginPath();
-        crc2.moveTo(735, 530);
-        crc2.lineTo(710, 580);
-        crc2.lineTo(760, 580);
-        crc2.closePath();
-        crc2.fillStyle = "#darkgreen";
-        crc2.fill();
-        crc2.fillStyle = "brown";
-        crc2.fillRect(820, 550, 10, 20);
-        crc2.beginPath();
-        crc2.moveTo(825, 470);
-        crc2.lineTo(850, 530);
-        crc2.lineTo(800, 530);
-        crc2.closePath();
-        crc2.closePath();
-        crc2.fillStyle = "darkgreen";
-        crc2.fill();
-        crc2.beginPath();
-        crc2.moveTo(825, 500);
-        crc2.lineTo(850, 550);
-        crc2.lineTo(800, 550);
-        crc2.closePath();
-        crc2.fillStyle = "#darkgreen";
         crc2.fill();
         /*Lift-Linie*/
         crc2.beginPath();
@@ -164,10 +137,9 @@ var aufgabe3;
             let y = 400 + Math.random() * 100;
             drawTree(x, y, "darkgreen");
         }
-        for (let i = 0; i < 50; i++) {
-            let x = 1 + Math.random() * 1000;
-            let y = 1 + Math.random() * 800;
-            drawFlakes(x, y, "white");
+        for (let i = 0; i < 100; i++) {
+            arrayX[i] = 0 + Math.random() * 1000;
+            arrayY[i] = 0 + Math.random() * 600;
         }
         /*H�tte*/
         crc2.fillStyle = "darkred";
@@ -183,6 +155,8 @@ var aufgabe3;
         crc2.closePath();
         crc2.fillStyle = "brown";
         crc2.fill();
+        canvasImg = crc2.getImageData(0, 0, 1000, 600); //Bild machen nachdem alles gezeichnet wurde
+        animate(); //Funktionsaufruf der Animation
     }
     function drawTree(_x, _y, _color) {
         crc2.fillStyle = "brown";
@@ -201,6 +175,39 @@ var aufgabe3;
         crc2.lineTo(_x - 25, _y + 90);
         crc2.closePath();
         crc2.fillStyle = _color;
+        /*Feste B�ume*/
+        crc2.fill();
+        crc2.fillStyle = "brown";
+        crc2.fillRect(820, 550, 10, 20);
+        crc2.beginPath();
+        crc2.moveTo(825, 470);
+        crc2.lineTo(850, 530);
+        crc2.lineTo(800, 530);
+        crc2.closePath();
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.moveTo(825, 500);
+        crc2.lineTo(850, 550);
+        crc2.lineTo(800, 550);
+        crc2.closePath();
+        crc2.fillStyle = "#darkgreen";
+        crc2.fill();
+        crc2.fillStyle = "brown";
+        crc2.fillRect(730, 580, 10, 20);
+        crc2.beginPath();
+        crc2.moveTo(735, 500);
+        crc2.lineTo(710, 560);
+        crc2.lineTo(760, 560);
+        crc2.closePath();
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.moveTo(735, 530);
+        crc2.lineTo(710, 580);
+        crc2.lineTo(760, 580);
+        crc2.closePath();
+        crc2.fillStyle = "#darkgreen";
         crc2.fill();
     }
     function drawFlakes(_x, _y, _color) {
@@ -210,6 +217,20 @@ var aufgabe3;
         crc2.fill();
         crc2.arc(_x, _y, 6, 0, 2 * Math.PI);
         crc2.stroke();
+    }
+    function animate() {
+        console.log("Timeout");
+        crc2.clearRect(0, 0, 1000, 600);
+        crc2.putImageData(canvasImg, 0, 0);
+        /*crc2.clearRect(0, 0, 400, 300);*/
+        for (let i = 0; i < arrayX.length; i++) {
+            if (arrayY[i] > 600) {
+                arrayY[i] = 0;
+            }
+            arrayY[i] += Math.random() + 10;
+            drawFlakes(arrayX[i], arrayY[i], "#000000");
+        }
+        window.setTimeout(animate, 20);
     }
 })(aufgabe3 || (aufgabe3 = {}));
 //# sourceMappingURL=skipiste.js.map
--- Aufgabe 03/skipiste.ts
+++ Aufgabe 03/skipiste.ts
@@ -9,14 +9,22 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 namespace aufgabe3 {
     window.addEventListener("load", init);
     let crc2: CanvasRenderingContext2D;
+    let x: number = 250;
+    let y: number = 250;
+    let arrayX: number[] = [];
+    let arrayY: number[] = [];
+    var canvasImg: any;
 
     function init(): void {
         let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
         console.log(canvas);
-
         crc2 = canvas.getContext("2d");
         console.log(crc2);
+        console.log("setTimeout");
 
+
+
+        /*Himmel - Canvas breiter als Aufgabenstellung*/
         crc2.fillStyle = "#b2dfee";
         crc2.fillRect(0, 0, 1000, 600);
 
@@ -31,41 +39,6 @@ namespace aufgabe3 {
         crc2.fillStyle = "#ffffff";
         crc2.fill();
 
-        /*Bäume*/
-        crc2.fillStyle = "brown";
-        crc2.fillRect(730, 580, 10, 20);
-        crc2.beginPath();
-        crc2.moveTo(735, 500);
-        crc2.lineTo(710, 560);
-        crc2.lineTo(760, 560);
-        crc2.closePath();
-        crc2.fillStyle = "darkgreen";
-        crc2.fill();
-        crc2.beginPath();
-        crc2.moveTo(735, 530);
-        crc2.lineTo(710, 580);
-        crc2.lineTo(760, 580);
-        crc2.closePath();
-        crc2.fillStyle = "#darkgreen";
-        crc2.fill();
-
-        crc2.fillStyle = "brown";
-        crc2.fillRect(820, 550, 10, 20);
-        crc2.beginPath();
-        crc2.moveTo(825, 470);
-        crc2.lineTo(850, 530);
-        crc2.lineTo(800, 530);
-        crc2.closePath();
-        crc2.closePath();
-        crc2.fillStyle = "darkgreen";
-        crc2.fill();
-        crc2.beginPath();
-        crc2.moveTo(825, 500);
-        crc2.lineTo(850, 550);
-        crc2.lineTo(800, 550);
-        crc2.closePath();
-        crc2.fillStyle = "#darkgreen";
-        crc2.fill();
 
         /*Lift-Linie*/
         crc2.beginPath();
@@ -95,8 +68,6 @@ namespace aufgabe3 {
         crc2.fillStyle = "#b2dfee";
         crc2.fillRect(505, 535, 40, 12);
 
-
-
         /*Wolke*/
         crc2.beginPath();
         crc2.arc(430, 110, 15, 0, 2 * Math.PI);
@@ -110,8 +81,6 @@ namespace aufgabe3 {
         crc2.arc(920, 105, 18, 0, 2 * Math.PI);
         crc2.fillStyle = "white";
         crc2.fill();
-
-
 
         /*Berge Links*/
         crc2.beginPath();
@@ -150,8 +119,7 @@ namespace aufgabe3 {
         crc2.fill();
         crc2.beginPath();
         crc2.arc(700, 110, 18, 0, 1 * Math.PI);
-        crc2.stroke()
-
+        crc2.stroke();
 
         /*Eis links*/
         crc2.beginPath();
@@ -190,10 +158,10 @@ namespace aufgabe3 {
             drawTree(x, y, "darkgreen");
         }
 
-        for (let i: number = 0; i < 50; i++) {
-            let x: number = 1 + Math.random() * 1000;
-            let y: number = 1 + Math.random() * 800;
-            drawFlakes(x, y, "white");
+        for (let i: number = 0; i < 100; i++) {
+            arrayX[i] = 0 + Math.random() * 1000;
+            arrayY[i] = 0 + Math.random() * 600;
+
 
         }
 
@@ -212,8 +180,9 @@ namespace aufgabe3 {
         crc2.closePath();
         crc2.fillStyle = "brown";
         crc2.fill();
+        canvasImg = crc2.getImageData(0, 0, 1000, 600);//Bild machen nachdem alles gezeichnet wurde
 
-
+        animate(); //Funktionsaufruf der Animation
     }
 
     function drawTree(_x: number, _y: number, _color: string): void {
@@ -234,9 +203,44 @@ namespace aufgabe3 {
         crc2.lineTo(_x - 25, _y + 90);
         crc2.closePath();
         crc2.fillStyle = _color;
+        
+        /*Feste Bäume*/
+        crc2.fill();
+        crc2.fillStyle = "brown";
+        crc2.fillRect(820, 550, 10, 20);
+        crc2.beginPath();
+        crc2.moveTo(825, 470);
+        crc2.lineTo(850, 530);
+        crc2.lineTo(800, 530);
+        crc2.closePath();
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.moveTo(825, 500);
+        crc2.lineTo(850, 550);
+        crc2.lineTo(800, 550);
+        crc2.closePath();
+        crc2.fillStyle = "#darkgreen";
+        crc2.fill();
+        crc2.fillStyle = "brown";
+        crc2.fillRect(730, 580, 10, 20);
+        crc2.beginPath();
+        crc2.moveTo(735, 500);
+        crc2.lineTo(710, 560);
+        crc2.lineTo(760, 560);
+        crc2.closePath();
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.beginPath();
+        crc2.moveTo(735, 530);
+        crc2.lineTo(710, 580);
+        crc2.lineTo(760, 580);
+        crc2.closePath();
+        crc2.fillStyle = "#darkgreen";
         crc2.fill();
 
     }
+
 
     function drawFlakes(_x: number, _y: number, _color: string): void {
 
@@ -247,10 +251,29 @@ namespace aufgabe3 {
         crc2.arc(_x, _y, 6, 0, 2 * Math.PI);
         crc2.stroke();
 
-
     }
 
 
+    function animate(): void {
+        console.log("Timeout");
+        crc2.clearRect(0, 0, 1000, 600);
+        crc2.putImageData(canvasImg, 0, 0);
+        /*crc2.clearRect(0, 0, 400, 300);*/
+
+        for (let i: number = 0; i < arrayX.length; i++) {
+            if (arrayY[i] > 600) {
+                arrayY[i] = 0;
+            }
+            arrayY[i] += Math.random() + 10;
+            drawFlakes(arrayX[i], arrayY[i], "#000000");
+
+        }
+
+        window.setTimeout(animate, 20);
+
+
+
+    }
 
 }
 
